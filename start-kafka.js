bd533d960944e7cfc610f85e9b3e5e6a748f2f01
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2026-02-05 16:12:27 +0530

    chore: update docker image

--- start-kafka.js
+++ start-kafka.js
@@ -2,4 +2,4 @@ const { execSync } = require('child_process');
 const path = require('path');
 
 const specmaticPath = path.resolve(__dirname, 'specmatic.yaml');
-execSync(`docker run --rm -v "${specmaticPath}:/usr/src/app/specmatic.yaml" -p 9092:9092 -p 9999:9999 specmatic/specmatic-kafka virtualize`, { stdio: 'inherit' });
+execSync(`docker run --rm -v "${specmaticPath}:/usr/src/app/specmatic.yaml" -p 9092:9092 -p 9999:9999 specmatic/enterprise mock`, { stdio: 'inherit' });
